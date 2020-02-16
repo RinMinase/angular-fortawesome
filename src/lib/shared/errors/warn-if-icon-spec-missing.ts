cbb7d8235131fd2e1bde8956571f7a17fb082589
Rin Minase <rin.black.raison.detre@gmail.com>
2020-02-16 08:23:29 +0800

    Removed module code

--- src/lib/shared/errors/warn-if-icon-spec-missing.ts
+++
@@ -1,3 +0,0 @@
-export const faWarnIfIconSpecMissing = () => {
-  throw new Error('Property `icon` is required for `fa-icon`/`fa-duotone-icon` components.');
-};
--- src/lib/shared/utils/is-icon-lookup.util.ts
+++
@@ -1,8 +0,0 @@
-import { IconLookup, IconProp } from '@fortawesome/fontawesome-svg-core';
-
-/**
- * Returns if is IconLookup or not.
- */
-export const isIconLookup = (i: IconProp): i is IconLookup => {
-  return (i as IconLookup).prefix !== undefined && (i as IconLookup).iconName !== undefined;
-};
